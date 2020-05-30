05f395b2683be5fea3846d2d240e5d023c23a330
Petko Pedev <petko.pedev@gmail.com>
2020-05-30 17:03:35 +0300

    Adding Animals task to the exercises.

---
+++ 02InheritanceExercises/Animals/Animal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private const string ERR_MSG = "Invalid input!";
+
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name 
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ERR_MSG);
+                }
+                this.name = value;
+            }
+        }
+        public int Age 
+        {
+            get
+            {
+                return this.age;
+            }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ERR_MSG);
+                }
+                this.age = value;
+            }
+        }
+        public string Gender 
+        {
+            get
+            {
+                return this.gender;
+            }
+            private set
+            {
+                if (value != "Male" && value != "Female")
+                {
+                    throw new ArgumentException(ERR_MSG);
+                }
+                this.gender = value;
+            }
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetType().Name}");
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            sb.AppendLine($"{this.ProduceSound()}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 02InheritanceExercises/Animals/Engine.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Animals
+{
+    public class Engine
+    {
+        private readonly List<Animal> animals;
+        public Engine()
+        {
+            this.animals = new List<Animal>();
+        }
+
+        public void Run()
+        {
+            string type;
+            while ((type = Console.ReadLine()) != "Beast!")
+            {
+                string[] animalArgs = Console.ReadLine().Split(' ').ToArray();
+                string name = animalArgs[0];
+                int age = int.Parse(animalArgs[1]);
+                string gender = null;
+
+                if (animalArgs.Length >= 3)
+                {
+                    gender = animalArgs[2];
+                }
+
+                Animal animal = null;
+
+                if (type == "Dog")
+                {
+                    animal = new Dog(name, age, gender);
+                }
+                else if (type == "Cat")
+                {
+                    animal = new Cat(name, age, gender);
+                }
+                else if (type == "Frog")
+                {
+                    animal = new Frog(name, age, gender);
+                }
+                else if (type == "Kitten")
+                {
+                    animal = new Kitten(name, age);
+                }
+                else if (type == "Tomcat")
+                {
+                    animal = new Tomcat(name, age);
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.animals.Add(animal);
+            }
+
+            foreach (Animal animal in this.animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
